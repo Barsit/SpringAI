8b1b63f9301ab78d270bda3ac20c497219e007da
db <1366022396@qq.com>
2024-07-10 16:44:44 +0800

    finish text

--- text/src/main/java/com/db/text/controller/TextController.java
+++ text/src/main/java/com/db/text/controller/TextController.java
@@ -1,6 +1,13 @@
 package com.db.text.controller;
 
 import jakarta.annotation.Resource;
+import org.springframework.ai.openai.OpenAiAudioSpeechModel;
+import org.springframework.ai.openai.OpenAiAudioTranscriptionModel;
+import org.springframework.ai.openai.audio.speech.SpeechPrompt;
+import org.springframework.ai.openai.audio.speech.SpeechResponse;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -14,5 +21,22 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TextController {
+    @Resource
+    private OpenAiAudioTranscriptionModel openAiAudioTranscriptionClient;
 
+    @Resource
+    private OpenAiAudioSpeechModel openAiAudioSpeechClient;
+    @RequestMapping(value = "/ai/text1")
+    private Object text1() {
+        ClassPathResource resource = new ClassPathResource("jkf.mp3");
+        String call = openAiAudioTranscriptionClient.call(resource);
+        System.out.println(call);
+        return call;
+    }
+    @RequestMapping(value = "/ai/text2")
+    private Object text2(@RequestParam(value = "msg") String msg) {
+        byte[] call = openAiAudioSpeechClient.call(msg);
+        System.out.println(call);
+        return "ok";
+    }
 }
