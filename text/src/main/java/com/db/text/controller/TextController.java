cf40b87b89a47da000efd6ead0d0d18ea6da65e0
db <1366022396@qq.com>
2024-07-10 11:10:41 +0800

    init text

---
+++ text/src/main/java/com/db/text/controller/TextController.java
@@ -0,0 +1,18 @@
+package com.db.text.controller;
+
+import jakarta.annotation.Resource;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @description:
+ * @projectName:SpringAI
+ * @see:com.db.text.controller
+ * @author:db
+ * @createTime:2024/7/10 11:01
+ * @version:1.0
+ */
+
+@RestController
+public class TextController {
+
+}
